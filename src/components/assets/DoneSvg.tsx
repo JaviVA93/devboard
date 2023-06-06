0af36795dc7668d4eb18184356e1c95a6eed1985
Javi Vilalr <javiroll93@gmail.com>
2023-06-06 13:18:07 +0200

    new distribution for the board, minor changes, done button for todos

---
+++ src/components/assets/DoneSvg.tsx
@@ -0,0 +1,25 @@
+
+export default function DoneSvg() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="20"
+      height="20"
+      fill="none"
+      viewBox="0 0 24 24"
+    >
+      <circle cx="12" cy="12" r="8" fill="#2A4157" fillOpacity="0.24"></circle>
+      <path
+        stroke="#222"
+        strokeWidth="1.2"
+        d="M8.5 11l2.894 2.894a.15.15 0 00.212 0L19.5 6"
+      ></path>
+      <path
+        stroke="#222"
+        strokeLinecap="round"
+        d="M19.358 10.546a7.5 7.5 0 11-3.608-5.041"
+      ></path>
+    </svg>
+  );
+}
+
--- src/components/to-do/issue.tsx
+++ src/components/to-do/issue.tsx
@@ -3,13 +3,15 @@
 import { useRef } from 'react';
 import style from './issue.module.css'
 import { gsap } from 'gsap'
+import Trash2Svg from '../assets/Trash2Svg';
+import DoneSvg from '../assets/DoneSvg';
 
 const Issue = (props: {
-        removeIssueOnList: Function,
-        id: string,
-        title: string,
-        text: string
-    }) => {
+    removeIssueOnList: Function,
+    id: string,
+    title: string,
+    text: string
+}) => {
 
     const { removeIssueOnList, id, title, text } = props;
     const card = useRef<HTMLDivElement | null>(null)
@@ -25,7 +27,7 @@ const Issue = (props: {
         tl.to(card.current, { width: 0, opacity: 0, duration: 0.25, ease: "power3.inOut" });
         tl.call(() => {
             if (props.id)
-            removeIssueOnList(id);
+                removeIssueOnList(id);
             tl.kill();
         });
 
@@ -33,11 +35,23 @@ const Issue = (props: {
     }
 
 
+    function markCardAsCompleted() {
+        if (!card.current) return;
+    }
+
+
     return (
         <div className={style.card} id={id} ref={card}>
             <h1>{title}</h1>
             <span>{text}</span>
-            <button onClick={removeCard}>Remove card</button>
+            <div className={style.ctasWrapper}>
+                <button className={style.removeBtn} onClick={removeCard}>
+                    <Trash2Svg />
+                </button>
+                <button className={style.completeBtn} onClick={markCardAsCompleted}>
+                    <DoneSvg />
+                </button>
+            </div>
         </div >
     )
 }
